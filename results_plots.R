fe010f802e42b63fe56e0e9820c9b9f75dfde3cb
lucasnell <lucnell@gmail.com>
2017-05-01 23:04:52 -0500

    Added alternative states plot

--- results_plots.R
+++ results_plots.R
@@ -1,10 +1,136 @@
-# Plots of potential results
+#' 
+#' # Plots of potential results
+#' 
 
 
 
 suppressPackageStartupMessages({
     library(tidyverse)
 })
+
+
+#' 
+#' <!-- ======================================================================== -->
+#' 
+#' <!-- Alternative states plot -->
+#' 
+#' <!-- ======================================================================== -->
+#' 
+#' 
+
+
+k1 = k2 = 100
+
+a12 = a21 = 2
+m1 = -1 / a12
+m2 = -a21
+
+# Non-stable equilibrium coordinates
+ns_equil <- c(x = {k1 - a12*k2} / {a12*(m2-m1)}, 
+              y = m2 * ({k1 - a12*k2} / {a12*(m2-m1)}) + k2)
+# Stable equilibrium coordinates
+equil <- list(sp1 = c(x = k1, y = 0), sp2 = c(x = 0, y = k2))
+
+# curve(m1 * x + (k1 / a12), 0, 100, xlim = c(0,k1), ylim = c(0,k1), 
+#       xlab = expression(N[1]), ylab = expression(N[2]))
+# curve(m2 * x + k2, 0, 100, add = TRUE, col = 'red')
+# points(x = ns_equil['x'], y = ns_equil['y'])
+
+# ----------
+# Creating data frame for red and blue lines (the "zero isoclines")
+# ----------
+line_df <- data_frame(x = c(seq(0, 100, length.out = 11))) %>% 
+    mutate(sp1 = m1 * x + (k1 / a12), sp2 = m2 * x + k2) %>% 
+    gather(spp, y, sp1:sp2) %>% 
+    mutate(spp = factor(spp)) %>% 
+    filter(y >= 0)
+
+
+
+
+# ----------
+# Creating ggplot object with no arrows
+# ----------
+no_arrow_p <- ggplot(line_df, aes(x, y)) +
+    theme_classic() +
+    # Red and blue lines
+    geom_line(aes(color = spp), size = 1) +
+    # Hacky axes
+    geom_path(data = data_frame(x = c(0, k1 + 10), y = c(0,0))) +
+    geom_path(data = data_frame(x = c(0,0), y = c(0, k2 + 10))) +
+    # Equilibrium points, first unstable, then stable
+    geom_point(data = data_frame(x = ns_equil['x'], y = ns_equil['y']),
+               shape = 21, color= 'black', fill = 'white', size = 3) +
+    geom_point(data = data_frame(x = c(k1, 0), y = c(0, k2), 
+                                 species = factor(c('sp1', 'sp2'))),
+               aes(color = species), size = 3, shape = 16) +
+    # Aesthetics
+    scale_x_continuous(expression(bold(N[1])), limits = c(0, k1 + 10),
+                       breaks = c(k2/a21, k1), 
+                       labels = c(expression(K[2] / alpha[21]), expression(K[1]))) +
+    scale_y_continuous(expression(bold(N[2])), limits = c(0, k2 + 10),
+                       breaks = c(k1 / a12, k2), 
+                       labels = c(expression(frac(K[1], alpha[12])), expression(K[2]))) +
+    scale_color_manual(values = rgb(c(86, 230), c(180,159), c(233, 0), 
+                                    maxColorValue = 255)) +
+    theme(axis.line = element_blank(), axis.ticks = element_blank(), 
+          axis.title = element_text(size = 14),
+          axis.text.x = element_text(margin = margin(-8, 0, 0, 0), size = 10, 
+                                     color = 'black'), 
+          axis.text.y = element_text(margin = margin(0, -10, 0, 0), size = 10,
+                                     color = 'black'), 
+          legend.position = 'none')
+
+
+no_arrow_p <- no_arrow_p + 
+    geom_text(data = line_df %>% 
+                  filter((spp == 'sp1' & x == 70) | (spp == 'sp2' & x == 20)), 
+              aes(label = paste('Zero isocline for species', gsub('sp', '', spp)),
+                  angle = ifelse(spp == 'sp1', {atan(m1) / (2 * pi)} * 360, 
+                                 {atan(m2) / (2 * pi)} * 360), 
+                  color = spp, x = x + 2.5, y = y + 2.5), 
+              size = 3, fontface = 'bold')
+no_arrow_p
+#
+
+
+
+
+# ggsave('~/Desktop/alt_states.pdf', no_arrow_p, width = 4, height = 4, units = 'in')
+
+
+
+# Not doing below bc it's very time-consuming
+# ----------
+# Creating data frame for arrows
+# ----------
+m12 <- {(k2 + k1/a12)/2} / {(k1 + k2/a21)/2}  # mean(c(m1, m2))
+b12 <- mean(c(k2, k1/a12))
+
+arrow_fun <- function(x, direction) {
+    if (direction == 'down') {
+        -m12 * (x - ns_equil['x']) + ns_equil['y']
+    }
+}
+
+# Arrows going downward from non-stable equilibrium
+arrow_df <- data_frame(x = round(ns_equil['x'] + 5), x2 = round(ns_equil['x'] + 11)) %>%
+    mutate(y = arrow_fun(x, 'down'), y2 = arrow_fun(x2, 'down'))
+
+# # Arrows going upward from non-stable equilibrium
+# arrow_df <- data_frame(x = round(ns_equil['x'] + 5), x2 = round(ns_equil['x'] + 11)) %>%
+#     mutate(y = m12*x + b12, y2 = m12*x2 + b12)
+
+
+p + geom_segment(data = arrow_df, aes(xend = x2, yend = y2),
+                 arrow = arrow(length = unit(0.02, "npc"), type = 'closed'))
+
+
+
+
+
+
+
 
 
 
@@ -125,29 +251,31 @@ attr_p
 #' 
 #' Scenarios:
 #' 
+#' 0. Random - No apparent pattern
 #' 1. Specialists - 3 spp
 #' 2. Generalist - 1 or more spp
-#' 3. Mixture - 2 or more spp
+#' 3. Alternative states - Switch btwn individual generalists
 #' 
 
 # Specialists are C, D, and E
 # Generalists are B and G
 
-scen_df <- read_csv('species,type,s0,s1,s2,s3
-A,other,0.1411,0.0,0.0,0.00
-B,generalist,0.0418,0.0,0.3,0.00
-C,specialist,0.0586,0.2,0.0,0.15
-D,specialist,0.0902,0.5,0.0,0.40
-E,specialist,0.143,0.3,0.0,0.25
-F,other,0.0318,0.0,0.0,0.00
-G,generalist,0.1415,0.0,0.7,0.20
-H,other,0.1488,0.0,0.0,0.00
-I,other,0.1041,0.0,0.0,0.00
-J,other,0.0991,0.0,0.0,0.00', col_types = 'ccdddd') %>% 
-    arrange(species)
-
-
-scen_df <- scen_df %>% gather(scen, abund, s0:s3)
+scen_df <- rbind(c('A','other',0.1411,0.0,0.0),
+    c('B','generalist',0.0418,0.0,0.3),
+    c('C','specialist',0.0586,0.2,0.0),
+    c('D','specialist',0.0902,0.5,0.0),
+    c('E','specialist',0.143,0.3,0.0),
+    c('F','other',0.0318,0.0,0.0),
+    c('G','generalist',0.1415,0.0,0.7),
+    c('H','other',0.1488,0.0,0.0),
+    c('I','other',0.1041,0.0,0.0),
+    c('J','other',0.0991,0.0,0.0)) %>% 
+    as_data_frame %>% 
+    rename_(.dots = setNames(paste0('V', 1:5), 
+                             c('species','type',paste0('s',0:2)))) %>% 
+    mutate_at(3:5, as.numeric) %>% 
+    arrange(species) %>%
+    gather(scen, abund, s0:s2)
 
 # # To replicate the random numbers under s0:
 # set.seed(0)
@@ -156,6 +284,8 @@ scen_df <- scen_df %>% gather(scen, abund, s0:s3)
 # i <- sample.int(length(x), 1)
 # x[i] <- x[i] - (sum(x) - 1)
 # sum(x); min(x)  # should be 1 and >0, respectively
+
+
 
 
 
@@ -174,34 +304,30 @@ scen_df %>%
     ylab('Abundance')
 
 
-scen_df %>% filter(scen == 's2', abund > 0)
+# Function to add noise to one row (i.e., one simulation/cage) of an abundance matrix
+randomize_matrix <- function(x, sim_sd){
+    y <- x[x > 0]
+    y <- y + rnorm(length(y), 0, sim_sd)
+    y <- ifelse(y > 0, y, 0)
+    # Dealing with zeros separately
+    z <- x[x == 0]
+    z <- z + (rbinom(length(z), 1, prob = sim_sd) * 0.01)
+    # Now make sure it adds to one
+    out <- x
+    out[x > 0] <- y
+    out[x == 0] <- z
+    out <- out / sum(out)
+    return(out)
+}
 
 
-scen_df %>% filter(scen == 's3', abund > 0)
-
-
-
-
-scen_fun <- function(scenario, sim_sd = 0.05, sim_N = 8) {
+scen_fun <- function(scenario, sim_sd = 0.2, sim_N = 8) {
 
     abunds <- scen_df %>% filter(scen == scenario) %>% arrange(species)
     
     mat <- matrix(rep(abunds$abund, sim_N), nrow = sim_N, byrow = TRUE)
     
-    ran_mat <- t(apply(mat, 1, function(x){
-        y <- x[x > 0]
-        y <- y + rnorm(length(y), 0, sim_sd)
-        y <- ifelse(y > 0, y, 0)
-        # Dealing with zeros separately
-        z <- x[x == 0]
-        z <- z + (rbinom(length(z), 1, prob = sim_sd) * 0.01)
-        # Now make sure it adds to one
-        out <- x
-        out[x > 0] <- y
-        out[x == 0] <- z
-        out <- out / sum(out)
-        return(out)
-    }))
+    ran_mat <- t(apply(mat, 1, randomize_matrix, sim_sd = sim_sd))
     
     ran_df <- as_data_frame(ran_mat)
     colnames(ran_df) <- unique(abunds$species)
@@ -211,14 +337,44 @@ scen_fun <- function(scenario, sim_sd = 0.05, sim_N = 8) {
                   cage = rep(1:sim_N, length(unique(abunds$species)))))
 }
 
+sim_alt_states <- function(states, sim_sd = 0.2, sim_N = 8){
+    abunds <- scen_df %>% filter(scen %in% states) %>% arrange(scen, species)
+    
+    ran_states <- sample(states, sim_N, replace = TRUE)
+    
+    ran_mat <- sapply(
+        ran_states,
+        function(s) {
+            mat <- matrix(scen_df$abund[scen_df$scen == s], nrow = 1)
+            randomize_matrix(mat, sim_sd)
+        }, USE.NAMES = FALSE)
+    ran_mat <- t(ran_mat)
+    
+    ran_df <- mutate(as_data_frame(ran_mat), cage = 1:sim_N, scen = 's3')
+    colnames(ran_df)[1:ncol(ran_mat)] <- unique(abunds$species)
+    ran_df <- gather(ran_df, species, abundance, -cage, -scen)
+    
+    return(ran_df)
+}
+
+
+
+
+
+
+
+
 
 set.seed(3)
-iscen_df <- lapply(unique(scen_df$scen), scen_fun, sim_sd = 0.2) %>% 
+iscen_df <- lapply(unique(scen_df$scen), scen_fun) %>% 
+    append(., list(sim_alt_states(c('s1', 's2')))) %>% 
     bind_rows %>% 
     arrange(species) %>% 
     mutate(species = factor(species), 
            scen = factor(scen, levels = paste0('s',0:3), 
-                         labels = c('Random', 'Specialists', 'Generalists', 'Mix')))
+                         labels = c('Random','Specialists','Generalists','Alt. States')))
+
+
 
 colors <- character(length(unique(scen_df$species)))
 colors[(scen_df$type == 'generalist')[1:10]] <- oranges
